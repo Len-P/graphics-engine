a92f0d932eed7acac421797923cd87b24d3607b4
Len-P <len.pasic2002@gmail.com>
2023-03-08 20:49:13 +0100

    Added directories and class files for 3D line drawings Added vector utilities

---
+++ src/LineDrawings3D/Figure3D.h
@@ -0,0 +1,20 @@
+#ifndef ENGINE_FIGURE3D_H
+#define ENGINE_FIGURE3D_H
+
+#include "../utils/ini_configuration.h"
+#include "../utils/easy_image.h"
+
+
+
+
+using namespace std;
+using namespace ini;
+using namespace img;
+
+namespace Figure3D {
+    class Figure3D {
+
+    };
+
+}
+#endif //ENGINE_FIGURE3D_H
